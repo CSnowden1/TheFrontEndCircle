23f1eb16826a3bc7fbc3a0c9687dc83b30e2c98d
CSnowden1 <snowden.chris@protonmail.com>
2023-12-16 02:17:38 -0500

    added firebase

--- frontend-circle/src/App.js
+++ frontend-circle/src/App.js
@@ -1,16 +1,26 @@
-import React, { useState, createContext } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, createContext, useContext } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import JobSubmissionPage from './pages/JobSubmissionPage';
 import JobBoardPage from './pages/JobBoardPage';
 import Navigation from './components/nav';
+import LoginForm from './components/LoginForm';
 
 
 
 export const UserContext = createContext(null);
 
+function ProtectedRoute({ children }) {
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+}
 
 function App() {
   const [user, setUser] = useState(null);
@@ -19,16 +29,20 @@ function App() {
     <UserContext.Provider value={{ user, setUser }}>
       <Router>
         <Header />
-        <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/submit-job" element={<JobSubmissionPage />} />
-        <Route path="/job-board" element={<JobBoardPage />} />
-      </Routes>
         <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/submit-job" element={
+            <ProtectedRoute>
+              <JobSubmissionPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/job-board" element={<JobBoardPage />} />
+          {/* Consider adding a route for the LoginForm */}
+        </Routes>
         <Footer />
       </Router>
     </UserContext.Provider>
-
   );
 }
 
---
+++ frontend-circle/src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { auth } from '../firebase-config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Logged in!");
+      // Redirect to home page or dashboard after login
+    } catch (error) {
+      console.error("Error logging in: ", error);
+    }
+  };
+ 
+  return (
+    <div>
+      <form onSubmit={login}>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
